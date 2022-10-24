1febf67863ac6437b7829d4de64531503bba822b
Mauro T Barone <mautot.barone@gmail.com>
2022-10-24 13:47:00 +0200

    iteration2to9 done

---
+++ src/components/Timestamp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Timestamp = (props) => {
+  return (
+    <>
+     <div className="timestamp"> {props.timestamp} </div>
+    </>
+  )
+}
+
+export default Timestamp

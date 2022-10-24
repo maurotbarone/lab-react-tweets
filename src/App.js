50f67f9d97213a4e816caf8d0348e06518439f5f
Mauro T Barone <mautot.barone@gmail.com>
2022-10-24 09:36:44 +0200

    iteration2 done

--- src/App.js
+++ src/App.js
@@ -34,10 +34,12 @@ const tweetsArray = [
   },
 ];
 
+
 function App() {
   return (
     <div className="App">
-      <Tweet />
+    <Tweet tweet={tweetsArray[0]}/>
+    {/* <Tweet {...tweetsArray[0]} /> */}
     </div>
   );
 }
